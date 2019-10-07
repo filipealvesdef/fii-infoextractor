28c1c1a49664ea124b16989fbbef724afaa14d51
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-10-07 00:30:19 -0300

    frontend: SearchTerm: add styles

--- frontend-web/src/SearchTerm.js
+++ frontend-web/src/SearchTerm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { makeStyles } from '@material-ui/styles';
 
 function SearchTerm(props) {
     const [currText, updateText] = useState(props.text);
@@ -43,13 +44,47 @@ function SearchTerm(props) {
         props.remove(props.id);
     }
 
-    return <div>
+    const classes = makeStyles({
+        searchTerm: {
+            margin: '0.5em',
+            display: 'inline-block',
+            borderRadius: '3px',
+            border: '1px solid #ccc',
+            '& div': {
+                display: 'inline',
+                padding: '0.5em',
+            },
+            '& input': {
+                background: '#dedede',
+                border: 'none',
+                borderRadius: '3px 0 0 3px',
+                marginRight: '0px',
+                padding: '0.5em',
+                width: (currText.length > 0 ? currText.length : 1) + 'ch',
+            },
+            '& input:focus': {
+                outline: 'none',
+            }
+        },
+        deleteBtn: {
+            background: '#dedede',
+            border: 'none',
+            borderRadius: '0 3px 3px 0',
+            padding: '0.5em',
+        }
+    })();
+
+    return <div className={classes.searchTerm}>
         {props.editing ? <input value={currText}
             onChange={e => {handleChange(e.target.value)}}
             onKeyDown={handleKeyDown}
             ref={inputRef}
         /> : <div onClick={e => {props.edit(props.id)}}>{currText}</div>}
-        <button onClick={deleteSearchTerm}>Delete me</button>
+        <button
+            onClick={deleteSearchTerm}
+            className={classes.deleteBtn}
+        >X
+        </button>
     </div>
 }
 
